9784f26ef5f85a8634fc1b663d301166e6511c4e
eduahcb <dev_eduardo@hotmail.com>
2022-03-01 14:41:31 -0300

    Adding editor config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,54 @@
+
+import GlobalStyle from '../src/styles/GlobalStyle'
+
+const withGlobalStyle = (StoryFn) => (
+  <>
+    <GlobalStyle />
+    <StoryFn />
+  </>
+)
+
+const viewports = {
+  extraSmall : {
+    name: 'Protrair phone (default)',
+    styles: {
+      width: '360px',
+      height: '640px'
+    }
+  },
+  small : {
+    name: 'Landscape phone (sm)',
+    styles: {
+      width: '640px',
+      height: '360px'
+    }
+  },
+  medium : {
+    name: 'Tablet (medium)',
+    styles: {
+      width: '768px',
+      height: '1024px'
+    }
+  },
+  large : {
+    name: 'Desktop (lg)',
+    styles: {
+      width: '1024px',
+      height: '1366px'
+    }
+  },
+  extraLarge : {
+    name: 'Large Desktop (xl)',
+    styles: {
+      width: '1280px',
+      height: '800px'
+    }
+  }
+}
+
+
+export const decorators = [withGlobalStyle]
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +57,8 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  layout: 'fullscreen',
+  viewport: {
+    viewports
+  }
 }
