2fc6e95bfa2019352874c7e0fb00ae57e2796c33
Eduardo <dev_eduardo@hotmail.com>
2022-03-01 17:52:20 -0300

    Creating BreakPoints e GlobalStyle stories (#3)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,8 @@
 module.exports = {
   "stories": [
     "../src/stories/*.stories.@(js|jsx|ts|tsx|mdx)",
-    "../src/components/*.stories.@(js|jsx|ts|tsx|mdx)"
+    "../src/components/*.stories.@(js|jsx|ts|tsx|mdx)",
+    "../src/styles/*.stories.@(js|jsx|ts|tsx|mdx)"
   ],
   "addons": [
     "@storybook/addon-links",
