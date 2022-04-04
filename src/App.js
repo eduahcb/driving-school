320ab93abae86f70dbcbffe9154f5530ef06a718
eduahcb <dev_eduardo@hotmail.com>
2022-04-04 18:38:51 -0300

    add /serivos and /sobre routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
 
 import ThemeProvider from 'styles/ThemeProvider'
 import GlobalStyle from 'styles/GlobalStyle'
 
-import Home from 'components/pages/Home'
+import Home from 'routes/Home'
+import About from 'routes/About'
+import ProductDetails from 'routes/ProductDetails'
 
 function App () {
   return (
     <ThemeProvider>
       <GlobalStyle />
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={ <Home />} />
+          <Route path="/sobre" element={<About />} />
+          <Route path="/servicos" element={<ProductDetails />} />
+        </Routes>
+      </Router>
     </ThemeProvider>
   )
 }
